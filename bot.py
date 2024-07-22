83cb06ff67dbb24d82b251413c983c0eab94cffb
Familysga <134228968+Familysga@users.noreply.github.com>
2024-07-22 14:15:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, idle
-#'‹ ٰ💸 ⇣ سورس الفراعنة ⇣ 💸 › .'#
+#'‹ ٰ💸 ⇣ سورس  ⇣ 💸 › .'#
 from pyromod import listen
 
 
@@ -8,14 +8,14 @@ bot = Client(
     "mo",
     api_id=8186557,
     api_hash="efd77b34c69c164ce158037ff5a0d117",
-    bot_token="5912908363:AAGkAqH3VTR0d-g0lAervmElqo9TqgDYLu4",#توكن المصنع
+    bot_token="6711048210:AAGeTTCLCXyBmcgcgC264YPkmI5g5xBJSnQ",#توكن المصنع
     plugins=dict(root="MHelal")
     )
 
 async def start_helalbot():
     print("تم تشغيل الصانع بنجاح..💗")
     await bot.start()
-    hh = "MohamedHelal_l"#يوزر المطور المصنع
+    hh = "H_8_o"#يوزر المطور المصنع
     try:
         await bot.send_message(hh, "**تم تشغيل الصانع بنجاح ...🥀**")
     except:
